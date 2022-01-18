ecdbcbb8356c810f61a2ac76b73285227f5f090e
Nathan <usmc_wolf@hotmail.com>
2022-01-18 16:59:46 -0600

    yea errors to fix

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String, nullable = False, unique = True)
     password = db.Column(db.String, nullable = False)
-    email = db.Column(db.String, nullable = False)
+    email = db.Column(db.String, unique = True)
 
     def __init__(self, username, password, email):
         self.username = username
